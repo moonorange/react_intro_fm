b7ee7fd8a1b070a5863e115a6ebf777ffd675cbe
moonorange <monoma632@gmail.com>
2021-06-27 18:55:40 +0900

    add: carousel componet to show images of the pet

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,9 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-	state = { loading: true };
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -27,9 +28,11 @@ class Details extends Component {
       return <h2>loading ...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
